ca428ae849f49a80d7db17515465ce2468c6313b
Stefan Stoichev <stefan.stoichev@gmail.com>
2017-11-07 07:31:49 +0000

    v1.2
    seems to work. more testing needed

--- jQuery-Slider.js
+++ jQuery-Slider.js
@@ -1,4 +1,4 @@
-define(["qlik", "jquery", './js/bootstrap.min', './js/jQuery.slider.custom', "./js/global", "text!./css/style.css", 'text!./css/jquery-ui.css', 'text!./css/scoped-bootstrap.css'], function (qlik, $, bootstrap, slidercustom, global, cssContent, jqueryUI, bootstrapCss) {
+define(["qlik", "jquery", './js/bootstrap.min', "text!./css/style.css", 'text!./css/jquery-ui.css', 'text!./css/scoped-bootstrap.css'], function (qlik, $, bootstrap, cssContent, jqueryUI, bootstrapCss) {
 	'use strict';
 	$("<style>").html(cssContent).appendTo("head");
 	$('<style>').html(bootstrapCss).appendTo('head');
@@ -61,15 +61,15 @@ define(["qlik", "jquery", './js/bootstrap.min', './js/jQuery.slider.custom', "./
 								value: "N",
 								label: "No frequency"
 							}, {
-									value: "V",
-									label: "Absolute value"
-								}, {
-									value: "P",
-									label: "Percent"
-								}, {
-									value: "R",
-									label: "Relative"
-								}],
+								value: "V",
+								label: "Absolute value"
+							}, {
+								value: "P",
+								label: "Percent"
+							}, {
+								value: "R",
+								label: "Relative"
+							}],
 							defaultValue: "V"
 						}
 					}
@@ -86,92 +86,174 @@ define(["qlik", "jquery", './js/bootstrap.min', './js/jQuery.slider.custom', "./
 			canTakeSnapshot: true
 		},
 		paint: function ($element, layout) {
+			var complete = false;
+			var me = this;
+			var lastrow = 0;
+			var data = [];
+			var qMatrix = [];
+			this.backendApi.eachDataRow(function (rownum, row) {
+				lastrow = rownum;
+				$.each(row, function (key, cell) {
+					// console.log(key,cell)
+				});
+			});
+
+
+			if (this.backendApi.getRowCount() > lastrow + 1) {
+				//we havent got all the rows yet, so get some more, 1000 rows
+				var requestPage = [{
+					qTop: lastrow + 1,
+					qLeft: 0,
+					qWidth: 10, //should be # of columns
+					qHeight: Math.min(1000, this.backendApi.getRowCount() - lastrow)
+				}];
+				this.backendApi.getData(requestPage).then(function (dataPages) {
+					//when we get the result trigger paint again
+					qMatrix = qMatrix.concat(dataPages[0].qMatrix);
+					me.paint($element);
+
+					// complete = true;
+					// console.log(complete)
+					// console.log(dataPages)
+					// console.log(data)
+				});
+			}
+
+
+
+
+			if (this.backendApi.getRowCount() == lastrow + 1) {
+				me.backendApi.eachDataRow(function (rownum, row) {
+					lastrow = rownum;
+					qMatrix.push(row[0]);
+					//console.log(rownum);
+					//do something with the row..
+				});
+
+
+				var app = qlik.currApp(this);
+				var field = this.backendApi.getDimensionInfos()[0].qFallbackTitle//layout.qListObject.qDimensionInfo.qFallbackTitle;
+				var object = qMatrix//layout.qListObject.qDataPages[0].qMatrix;
+
+				var selPosVals = [];
+				for (var i = 0; i < object.length; i++) {
+					if (object[i].qState != "X" && object[i].qState != "A") {
+						selPosVals.push(parseInt(object[i].qElemNumber));
+					} else {
+
+					}
+				}
+
+				selPosVals.sort(sortNumber);
+
+				var consecutive = true;
+				if (selPosVals.length != 1) {
+					for (var i = 0; i < selPosVals.length - 1; i++) {
+						if (selPosVals[i + 1] - selPosVals[i] == 1) {
+							consecutive = true;
+						} else {
+							consecutive = false;
+							break;
+						}
+					}
+				}
+
+				var sliderVals = [];
+				if (consecutive == true) {
+					sliderVals.push(selPosVals[0], selPosVals[selPosVals.length - 1]);
+				} else {
+
+				}
+
+				app.getList("SelectionObject", function (reply) {
+
+					var selectedFields = reply.qSelectionObject.qSelections;
+					for (var i = 0; i < selectedFields.length; i++) {
+						if (selectedFields[i].qField === field) {
+
+						}
+					}
+				})
+
+				var self = this //, html = "<ul>";
+				var html = '<div style="margin: 15px; padding: 25px"> <div id="slider-range"></div> </div> <span id="sliderMsg"></span>'
+				// <span id="sliderStart"></span><span>&nbsp;&nbsp;&nbsp;&nbsp;</span><span id="sliderEnd"></span></span>	
+				var data = qMatrix; //layout.qListObject.qDataPages[0].qMatrix
+				// console.log(layout.qListObject)
+
+				data.sort(function (a, b) {
+					//console.log(a[0])
+					if (a.qElemNumber < b.qElemNumber) return -1;
+					if (a.qElemNumber > b.qElemNumber) return 1;
+					return 0;
+				})
+
+				var sliderData = [];
+				for (var i = 0; i < data.length; i++) {
+					sliderData.push(data[i].qText);
+				}
+
+
+
+				console.log(lastrow)
+				//console.log(data.length)
+
+
+
+				$element.html(html);
+				$("#slider-range").slider({
+					range: true,
+					min: 0,
+					max: data.length - 1,
+					//values: sliderVals,
+					stop: function (event, ui) {
+						//$("#sliderMsg").text( '( ' + (ui.values[1] - ui.values[0]) + ' values selected)' );
+						var toSelect = []
+						for (var i = ui.values[0]; i < ui.values[1] + 1; i++) {
+							toSelect.push(i);
+						}
+
+						setTimeout(function () {
+							$(ui.handle).attr('title', sliderData[ui.value]).tooltip('fixTitle').tooltip('show');
+						}, 5);
+
+						self.backendApi.selectValues(0, toSelect, false);
+					},
+					slide: function (event, ui) {
+						setTimeout(function () {
+							$(ui.handle).attr('title', sliderData[ui.value]).tooltip('fixTitle').tooltip('show');
+						}, 5);
+					},
+					change: function (event, ui) {
+
+						if (consecutive == true) {
+							setTimeout(function () {
+								$(ui.handle).attr('title', sliderData[ui.value]).tooltip('fixTitle').tooltip('show');
+							}, 5);
+						}
+					},
+					create: function (event, ui) {
+						create(event, ui, $(this).slider('values', 0), $(this).slider('values', 1))
+					}
+
+				});
+
+				if (consecutive == true) {
+					$("#slider-range").slider("values", sliderVals);
+				} else {
+
+					$("#sliderMsg").text('Non consecutive values are selected/possible');
+				}
+			}
+			// console.log(dataPages)
+			/*
+
+			*/
 
 			function sortNumber(a, b) {
 				return a - b;
 			}
 
-			var app = qlik.currApp(this);
-			var field = layout.qListObject.qDimensionInfo.qFallbackTitle;
-			var object = layout.qListObject.qDataPages[0].qMatrix;
-			//console.log(object)
-
-			var selPosVals = [];
-			//console.log(object)
-			for (var i = 0; i < object.length; i++) {
-				if (object[i][0].qState != "X" && object[i][0].qState != "A") {
-					//console.log(object[i][0].qState)
-					//console.log( object[i][0].qElemNumber)						
-					selPosVals.push(parseInt(object[i][0].qElemNumber));
-					//console.log(selPosVals)
-				} else {
-
-				}
-			}
-
-			selPosVals.sort(sortNumber);
-
-			var consecutive = true;
-			if (selPosVals.length != 1) {
-				for (var i = 0; i < selPosVals.length - 1; i++) {
-					if (selPosVals[i + 1] - selPosVals[i] == 1) {
-						consecutive = true;
-					} else {
-						//console.log(selPosVals[i + 1] + ' ' + selPosVals[i])
-						consecutive = false;
-						//console.log('false')
-						break;
-					}
-				}
-			}
-
-			var sliderVals = [];
-			if (consecutive == true) {
-				//console.log(sliderVals)
-				sliderVals.push(selPosVals[0], selPosVals[selPosVals.length - 1]);
-			} else {
-				//console.log('false')
-			}
-			//console.log(selPosVals);			
-			//console.log(sliderVals)
-
-			//console.log(sliderVals)
-			//$( "#slider-range" ).slider( "values", sliderVals[0], sliderVals[1] );
-			//console.log( $( "#slider-range" ).slider( "values", 0 ) );
-
-			app.getList("SelectionObject", function (reply) {
-
-				var selectedFields = reply.qSelectionObject.qSelections;
-				for (var i = 0; i < selectedFields.length; i++) {
-					if (selectedFields[i].qField === field) {
-						//min = selectedFields[i]
-						//console.log(selectedFields[i])
-					}
-				}
-			})
-
-			var self = this //, html = "<ul>";
-			var html = '<div style="margin: 15px; padding: 25px"> <div id="slider-range"></div> </div> <span id="sliderMsg"></span>'
-			// <span id="sliderStart"></span><span>&nbsp;&nbsp;&nbsp;&nbsp;</span><span id="sliderEnd"></span></span>	
-			var data = layout.qListObject.qDataPages[0].qMatrix
-
-			data.sort(function (a, b) {
-				//console.log(a[0])
-				if (a[0].qElemNumber < b[0].qElemNumber) return -1;
-				if (a[0].qElemNumber > b[0].qElemNumber) return 1;
-				return 0;
-			})
-
-
-			for (var i = 0; i < data.length; i++) {
-				sliderData.push(data[i][0].qText);
-			}
-			
-			// this.backendApi.eachDataRow(function (rownum, row) {
-
-			// });
-			//console.log(data.length)
-			
 			function create(event, ui, start, end) {
 				var handles = $(event.target).find('span');
 				handles.eq(0).tooltip({
@@ -190,58 +272,7 @@ define(["qlik", "jquery", './js/bootstrap.min', './js/jQuery.slider.custom', "./
 				}).tooltip('show');
 			}
 
-			$element.html(html);
-			$("#slider-range").dragslider({
-				range: true,
-				min: 0,
-				max: data.length - 1,
-				rangeDrag: true,
-				//values: sliderVals,
-				stop: function (event, ui) {
-					//$("#sliderMsg").text( '( ' + (ui.values[1] - ui.values[0]) + ' values selected)' );
-					var toSelect = []
-					for (var i = ui.values[0]; i < ui.values[1] + 1; i++) {
-						toSelect.push(i);
-					}
 
-					dragging = false;
-
-					setTimeout(function () {
-						$(ui.handle).attr('title', sliderData[ui.value]).tooltip('fixTitle').tooltip('show');
-					}, 5);
-
-					self.backendApi.selectValues(0, toSelect, false);
-				},
-				slide: function (event, ui) {
-					if (dragging == false) {
-						setTimeout(function () {
-							//console.log('test')
-							$(ui.handle).attr('title', sliderData[ui.value]).tooltip('fixTitle').tooltip('show');
-							//console.log(sliderData[ui.value])
-						}, 5);
-					}
-				},
-				change: function (event, ui) {
-					if (dragging == false) {
-						if (consecutive == true) {
-							setTimeout(function () {
-								$(ui.handle).attr('title', sliderData[ui.value]).tooltip('fixTitle').tooltip('show');
-							}, 5);
-						}
-					}
-				},
-				create: function (event, ui) {
-					create(event, ui, $(this).dragslider('values', 0), $(this).dragslider('values', 1))
-				}
-
-			});
-
-			if (consecutive == true) {
-				$("#slider-range").dragslider("values", sliderVals);
-			} else {
-
-				$("#sliderMsg").text('Non consecutive values are selected/possible');
-			}
 		}
 	};
 });
